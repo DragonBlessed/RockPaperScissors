57c9d6579784d20e27caaa7b63e531ecea79482d
DragonBlessed <christianvasil72@gmail.com>
2022-12-24 01:51:14 -0800

    Update RPS.js

--- RPS.js
+++ RPS.js
@@ -1,27 +1,61 @@
 function getComputerChoice() {
-    const number = getRandomNumber()
-
-    switch (number) {
-        case 0:
-            return 'ROCK';
-            break;
-        case 1:
-            return 'PAPER';
-            break;
-        case 2:
-            return 'SCISSORS';
-            break;
-        default:
-            console.log("Sorry! Something went wrong!")
-    }
+    var Choices = [
+        'Scissors',
+        'Rock',
+        'Paper'
+    ];
+    var randomChoice = Math.floor(Math.random()*Choices.length);
+    return randomChoice;
 }
+
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection = 'ROCK' && (computerSelection = 'ROCK')) {
-    return "Tie. Both used rock."}
+    getComputerChoice()
+    for (choice in Choices) {
+        if (playerSelection == 'Scissors') {
+            if (computerSelection == 'Paper') {
+                return "You win! Paper beats scissors!"
+            }
+            else if (computerSelection == 'Rock') {
+                return "You lose! Rock beats scissors!"
+            }
+            else if (computerSelection == 'Scissors') {
+                return "Tie! Scissors + Scissors are too evenly matched!"
+            }
+        }
+        else if (playerSelection == 'Paper') {
+            if (computerSelection == 'Scissors') {
+                return "You lose! Scissors beats paper!"
+            }
+            else if (computerSelection == 'Rock') {
+                return "You win! Paper beats rock!"
+            }
+            else if (computerSelection == 'Paper') {
+                return "Tie! Paper + Paper are too evenly matched!"
+            }    
+        }
+        else if (playerSelection == 'Rock') {
+            if (computerSelection == 'Scissors') {
+                return "You win! Rock beats scissors!"
+            }
+            else if (computerSelection == 'Paper') {
+                return "You lose! Paper beats rock!"
+            }
+            else if (computerSelection == 'Rock') {
+                return "Tie! Rock + Rock are too evenly matched!"
+            }    
+        }
+    }}
 
-}
 
-const playerSelection = string1.toUpperCase("rock") === string2.toUpperCase("ROCK");
+let playerSelection = prompt('What do you throw?')
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for (let i = 0; i < 5; i++) { 
+        getComputerChoice()
+        playRound(playerSelection, computerSelection)
+    }
+}
