f74dbd5cfcbfb99f8fa310a10c3a676b190e00af
DragonBlessed <christianvasil72@gmail.com>
2023-06-02 03:40:09 -0700

    Finished gameplay. May/may not modify visuals

--- RPS.js
+++ RPS.js
@@ -19,49 +19,122 @@ function getComputerChoice() {
 }
 
 
-function playRound(playerSelection) {
-    getComputerChoice()
-    let computerSelection = getComputerChoice;
-        if (playerSelection == 'Scissors') {
-            if (computerSelection == 'Paper') {
-                return "You win! Paper beats scissors!"
-            }
-            else if (computerSelection == 'Rock') {
-                return "You lose! Rock beats scissors!"
-            }
-            else if (computerSelection == 'Scissors') {
-                return "Tie! Scissors + Scissors are too evenly matched!"
-            }
-        }
-        else if (playerSelection == 'Paper') {
-            if (computerSelection == 'Scissors') {
-                return "You lose! Scissors beats paper!"
-            }
-            else if (computerSelection == 'Rock') {
-                return "You win! Paper beats rock!"
-            }
-            else if (computerSelection == 'Paper') {
-                return "Tie! Paper + Paper are too evenly matched!"
-            }    
-        }
-        else if (playerSelection == 'Rock') {
-            if (computerSelection == 'Scissors') {
-                return "You win! Rock beats scissors!"
-            }
-            else if (computerSelection == 'Paper') {
-                return "You lose! Paper beats rock!"
-            }
-            else if (computerSelection == 'Rock') {
-                return "Tie! Rock + Rock are too evenly matched!"
-            }    
-        }
+
+function playRound(playerSelection, computerSelection, playerPoints, computerPoints) {
+    if (playerSelection === 'Scissors') {
+      if (computerSelection === 'Paper') {
+        playerPoints++;
+        return {
+          message: "You win! Paper beats scissors!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      } else if (computerSelection === 'Rock') {
+        computerPoints++;
+        return {
+          message: "You lose! Rock beats scissors!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      } else if (computerSelection === 'Scissors') {
+        return {
+          message: "Tie! Scissors + Scissors are too evenly matched!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      }
+    } else if (playerSelection === 'Paper') {
+      if (computerSelection === 'Scissors') {
+        computerPoints++;
+        return {
+          message: "You lose! Scissors beats paper!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      } else if (computerSelection === 'Rock') {
+        playerPoints++;
+        return {
+          message: "You win! Paper beats rock!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      } else if (computerSelection === 'Paper') {
+        return {
+          message: "Tie! Paper + Paper are too evenly matched!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      }
+    } else if (playerSelection === 'Rock') {
+      if (computerSelection === 'Scissors') {
+        playerPoints++;
+        return {
+          message: "You win! Rock beats scissors!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      } else if (computerSelection === 'Paper') {
+        computerPoints++;
+        return {
+          message: "You lose! Paper beats rock!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      } else if (computerSelection === 'Rock') {
+        return {
+          message: "Tie! Rock + Rock are too evenly matched!",
+          playerPoints: playerPoints,
+          computerPoints: computerPoints
+        };
+      }
     }
+  }
+
+var playerSelection;
+var playerPoints = 0;
+var computerPoints = 0;
+var  gameOver = false;
 
 
-let playerSelection = document.getElementsByClassName("choices").addEventListener("click", );
-const computerSelection = getComputerChoice();
-//console.log(playRound(playerSelection, computerSelection
-console.log(playerSelection, computerSelection);
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (!gameOver) { 
+        playerSelection = button.id;
+        var computerSelection = getComputerChoice();
+        var result = playRound(playerSelection, computerSelection, playerPoints, computerPoints);
+        playerPoints = result.playerPoints;
+        computerPoints = result.computerPoints;
+        displayResult(result.message);
+        displayScore(playerPoints, computerPoints);
+        if (playerPoints === 5 || computerPoints === 5) {
+            gameOver = true;
+            endGame();
+            buttons.forEach((btn) => {
+              btn.removeEventListener('click');
+            
+            
+        });
+    }
+    }
+});
+});
 
+function displayResult(result) {
+    var resultContainer = document.getElementById('result');
+    resultContainer.textContent = result;
+  }
+
+function displayScore(playerPoints, computerPoints) {
+    var scoreContainer = document.getElementById('score');
+    scoreContainer.textContent = "The score is now " + computerPoints + " - " + playerPoints + "!";
+  }
+
+  function endGame() {
+    const h1 = document.createElement("h1");
+    const ending = document.createTextNode("GAME OVER!");
+    h1.appendChild(ending);
+    document.getElementById("endgame").appendChild(h1);
+  }
 
 
